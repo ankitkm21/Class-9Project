d7278e827dcb983049fa2a90fb1ea780c58a21be
nithyaKo <86623961+nithyaKo@users.noreply.github.com>
2021-07-06 10:11:02 -0400

    Add files via upload

---
+++ Project_Template_C9_Background_Color_Changer-main/sketch.js
@@ -0,0 +1,46 @@
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400); 
+  
+  btn_red= createButton("RED");
+  btn_red.position(100,50)
+  btn_red.mousePressed(red_bg)
+
+
+  btn_green= createButton("GREEN");
+  btn_green.position(250,50)
+  btn_green.mousePressed(green_bg)
+}
+
+function draw() {
+
+  if (btn_green.mousePressed){
+    background("green")
+  }
+
+  if (btn_red.mousePressed){
+    background("red")
+  }
+
+  background("white");
+}
+
+
+function red_bg(){
+  r=255
+  g=0
+  b=0
+}
+
+function green_bg(){
+  r=0
+  g=255
+  b=0
+}
+
